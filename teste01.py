d73ffc23aa549be9226a2f14f63a1c2a3af9c601
Caroline Araujo <ca.araujosouza@gmail.com>
2020-11-14 10:30:04 -0300

    first commit

---
+++ teste01.py
@@ -0,0 +1,16 @@
+nome = input("Qual é o seu nome?")
+idade = input("Qual é a sua idade?")
+peso = input("Qual é o seu peso?")
+mensagem = input("Qual é o seu nome??")
+print("Olá " + mensagem + "! Prazer em te conhecer")
+Dia = 8
+Mes = "Junho"
+Ano = 1993
+print("Você nasceu no", Dia, " de",  Mes, " de",  Ano, ". Correto?")
+num1 = int(input("Digite um número"))
+num2 = int(input("Digite outro número"))
+soma = num1 + num2
+print(int(soma))
+
+
+
